652b1b64eadedb3996505a11a1caa2beeacd4a49
Yang Li-Yi <joseph.lyyang@gmail.com>
2015-09-15 11:42:29 +0800

    Update flow_control/ex3.rb

--- flow_control/ex3.rb
+++ flow_control/ex3.rb
@@ -1,16 +1,13 @@
-def num_taker (num)
-  if num < 0
-    return "Your number is lower than 0!"
-  elsif num <= 50
-    return "Your number is between 0~50!"
-  elsif num <= 100
-    return "Your number is between 51~100!"
-  else
-    return "Your number is above 100!"
-  end
+puts "Give me a number:"
+num = gets.to_i
+
+if num < 0
+  puts "Your number is lower than 0!"
+elsif num <= 50
+  puts "Your number is between 0~50!"
+elsif num <= 100
+  puts "Your number is between 51~100!"
+else
+  puts "Your number is above 100!"
 end
 
-puts num_taker(6)
-puts num_taker(70)
-puts num_taker(-2)
-puts num_taker(104)
