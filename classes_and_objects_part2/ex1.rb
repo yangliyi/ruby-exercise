b27e86a9d4e949800d9093dcdd98570852c23448
Yang Li-Yi <joseph.lyyang@gmail.com>
2015-09-15 15:06:13 +0800

    Update ex2.rb

---
+++ classes_and_objects_part2/ex1.rb
@@ -0,0 +1,32 @@
+class MyCar
+  attr_accessor :color
+    attr_reader :speed, :year
+  def initialize(year, color, model)
+    @year = year
+    @color = color
+    @model = model
+    @speed = 0
+  end
+  def self.miles_gas_calculate(miles, gas)
+    puts "your m/g rate is #{miles.to_f / gas.to_f}"
+  end
+  def speedup(plus)
+    @speed += plus
+  end
+  def brake(minus)
+    @speed -= minus
+  end
+  def shut_off
+    @speed = 0
+  end
+  def spray_paint(color)
+    self.color = color
+  end
+end
+
+beauty = MyCar.new("2015", "sky blue", "sportcar")
+beauty.color = "red"
+beauty.speedup(130)
+beauty.brake(20)
+beauty.speed
+beauty.shut_off
