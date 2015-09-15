f4532e0effae8ec51b5780def1309d9774ab6ef6
Yang Li-Yi <joseph.lyyang@gmail.com>
2015-09-15 15:36:39 +0800

    Update inherit/ex1.rb

---
+++ inheritance/ex1.rb
@@ -0,0 +1,42 @@
+class Vehicle
+  def self.miles_gas_calculate(miles, gas)
+    puts "your m/g rate is #{miles.to_f / gas.to_f}"
+  end
+end
+class MyCar < Vehicle
+  NUMBER_OF_SEATS = 4
+  attr_accessor :color
+    attr_reader :speed, :year
+  def initialize(year, color, model)
+    @year = year
+    @color = color
+    @model = model
+    @speed = 0
+  end
+  def to_s
+    "my car is #{self.color}, #{@year}, #{@model}"
+  end
+  def speedup(plus)
+    @speed += plus
+  end
+  def brake(minus)
+    @speed -= minus
+  end
+  def shut_off
+    @speed = 0
+  end
+  def spray_paint(color)
+    self.color = color
+  end
+end
+
+class MyTruck < Vehicle
+  NUMBER_OF_SEATS = 4
+end
+
+beauty = MyCar.new("2015", "sky blue", "sportcar")
+beauty.color = "red"
+beauty.speedup(130)
+beauty.brake(20)
+beauty.speed
+beauty.shut_off
